abd65dde57aec41bc0955c1e67c8370dd52c64a6
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2024-03-30 09:51:32 +0000

    fix bug, browserAction -> action

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener(() => {
+chrome.action.onClicked.addListener(() => {
   chrome.bookmarks.search({ 'title': 'Reading list' }, (results) => {
     console.log(results)
 
